cfa7ef0e7593cdc08d5ae3be4d68bf9b2cc76bf3
Fouad Sakr <fouad.sakr@aptiv.com>
2026-01-06 09:38:49 +0100

    Pin aesd-assignments package to init-script commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 # NOTE: Repository contains your aesd assignments content including the `server/` directory.
 # Pin to a specific commit for reproducible builds.
-AESD_ASSIGNMENTS_VERSION = main
+AESD_ASSIGNMENTS_VERSION = f50a23a1fabf07fbf69b8a9c6ccd53f9770ab65c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
